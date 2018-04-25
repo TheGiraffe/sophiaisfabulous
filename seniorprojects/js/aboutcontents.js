b1aca3c788d3f9d45f389d63720d4ae30c55907f
GiraffeBot <TheGiraffe@users.noreply.github.com>
2018-04-24 15:33:22 -1000

    update, now the most recent site

---
+++ seniorprojects/js/aboutcontents.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('#aboutmedescription').hide();
+  $('#aboutme').on('click', function(){
+$('#aboutmedescription').show();
+  });
+
+  $('#aboutme').on('dblclick', function(){
+  $('#aboutmedescription').hide();
+  });
+  });
--- seniorprojects/js/contents.js
+++ seniorprojects/js/contents.js
@@ -1,177 +1,120 @@
-var content1=
-('<br><h3>TRIANGULAR BOOKSHELF</h3><br>')+
-('<div class="contentlandinggriddescription">ONE OF MY FIRST SERIOUS WOODWORKING PROJECTS </div>')+
-('<div class="contentlandinggriddescription">')+
-('Built using scrap wood from my old bed. Partially inspired by Scandinavian architecture/design.<br></div>');
-var content1short=
-('<br><h3>TRIANGULAR BOOKSHELF</h3><br>')+
-('<div class="contentlandinggriddescription">ONE OF MY FIRST SERIOUS WOODWORKING PROJECTS. </div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+$(function () {
+  $('#contact').on('click', function(){
+  $('.description').html(contact);
+});
+});
+var contact=('<h3>CONTACT INFORMATION</h3><br>')+('Phone: 808-214-2893<br>School Email: sdavis@kiheicharter.org<br>Personal Email: sodavis10@gmail.com');
 
-var content2=
-('<br><h3>LIGHT UP PARTS ORGANIZER</h3><br>')+
-('<div class="contentlandinggriddescription">A WORK IN PROGRESS </div>')+
-('<div class="contentlandinggriddescription">')+
-('Bringing beauty to the workbench. Part of an ongoing project to revamp my workspace. When it is finished, it will be a beautiful glowing box of hardware materials. <br></div>');
-var content2short=
-('<br><h3>LIGHT UP PARTS ORGANIZER</h3><br>')+
-('<div class="contentlandinggriddescription">A WORK IN PROGRESS</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+$(function () {
+  $('#goals').on('click', function(){
+  $('.description').html(goals);
+});
+});
+var goals=('<h3>ACADEMIC AND PERSONAL OBJECTIVES</h3><br>')+('I would like to major in Biomedical Engineering in college and obtain a Ph.D. I would also like to attend medical school and specialize in pathology. I hope to achieve this through an MD Ph.D program at a research university. After grad school, I want to combine my interests in engineering and medicine to create a biomedical technologies startup. After having a startup and other engineering experience, I would like to become a professor at a research university.');
 
-var content3=
-('<br><h3>MICROSCOPIC PHOTOGRAPHY: COSMETICS</h3><br>')+
-('<div class="contentlandinggriddescription">LOOKING AT COSMETICS UNDER A MICROSCOPE </div>')+
-('<div class="contentlandinggriddescription">')+
-('A microscopic photography project looking at common cosmetics such as nail polish, lip stain, cream eyeshadow, and others.<br></div>')+
-('<div class="contentlandinggriddescription"><a href="https://flic.kr/s/aHskABUWF6"><i>MICROSCOPIC PHOTOGRAPHY GALLERY</i></a></div>');
-var content3short=
-('<br><h3>MICROSCOPIC PHOTOGRAPHY: COSMETICS</h3><br>')+
-('<div class="contentlandinggriddescription">LOOKING AT COSMETICS UNDER A MICROSCOPE </div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
 
-var content4=
-('<br><h3>LED FLOWERS</h3><br>')+
-('<div class="contentlandinggriddescription">BRIGHTEN UP SOMEONES DAY</div>')+
-('<div class="contentlandinggriddescription">')+
-('Short, fun project making light up flowers. Origami carnations with LEDs in the centers, wires running through the stems to the battery pack located on the bottom cork.<br></div>');
+var gfb=('June 2017 - July 2017 <br> Participated in the Women In Technology Stemworks Summer Internship and was assigned to Goodfellow Brothers, Inc. At Goodfellow, I learned about civil engineering and surveying. I worked on 3D models for sites around Maui and gained lots of surveying experience.');
+var kpl=('March 2016 - March 2017 <br> Worked at Kihei Public Library for a year as a stuent helper. Duties included arranging, shelving, stamping, and labeling books. Also helped set up the kids crafts and did various assignments throughout the library.');
+var nf=('June 2016 - July 2016 <br> Worked with the Programming team for the new Nalukai website. Wrote some of the code (HTML, CSS) and inserted comments to help explain the code to future Nalukai campers.');
+var mig=('June 2015 - July 2015 <br> Participated in the Women In Technology Stemworks Summer Internship and was assigned to Maui Innovation Group. Worked on the electronics and android app for Maui Innovation Group&#39;s coconut harvesting robot.');
+$(function () {
+  $('#work').on('click', function(){
+  $('.description').html(work);
+  $('#g').hide();
+  $('#k').hide();
+  $('#n').hide();
+  $('#m').hide();
+  $(function () {
+    $('#goodfellowbutton').on('click', function(){
+$('#g').html(gfb).show();
+$('#k').hide();
+$('#n').hide();
+$('#m').hide();
+  });
+  $('#kiheipubliclibrarybutton').on('click', function(){
+$('#k').html(kpl).show();
+$('#g').hide();
+$('#n').hide();
+$('#m').hide();
+});
+$('#nalukaibutton').on('click', function(){
+$('#n').html(nf).show();
+$('#g').hide();
+$('#k').hide();
+$('#m').hide();
+});
+$('#mauiinnovationbutton').on('click', function(){
+$('#m').html(mig).show();
+$('#g').hide();
+$('#k').hide();
+$('#n').hide();
+});
+  });
+});
+});
 
-var content4short=
-('<br><h3>LED FLOWERS</h3><br>')+
-('<div class="contentlandinggriddescription">BRIGHTEN UP SOMEONES DAY</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+var work=('<h3>WORK EXPERIENCE</h3><br>')+('<ul class="works"><li><button class="moreinfo" id="goodfellowbutton"><span>Summer Intern at Goodfellow Bros, Inc.</span></button></li>')+('<div class="wdesc" id="g"></div>')+('<li><button class="moreinfo" id="kiheipubliclibrarybutton"><span>Student Helper I at Kihei Public Library</span></button></li>')+('<div class="wdesc" id="k"></div>')+('<li><button class="moreinfo" id="nalukaibutton"><span>Intern at Nalukai Foundation</span></button></li>')+('<div class="wdesc" id="n"></div>')+('<li><button class="moreinfo" id="mauiinnovationbutton"><span>Summer Intern at Maui Innovation Group</span></button></li>')+('<div class="wdesc" id="m"></div>')+('</ul>');
 
-var content5=
-('<br><h3>ROBOTIC ENDOSCOPE CAM</h3><br>')+
-('<div class="contentlandinggriddescription">IT IS WATCHING YOU. :) </div>')+
-('<div class="contentlandinggriddescription">')+
-('A short project testing some servos, which morphed into an amusing exercise. Used the endoscope camera to "scan" my workspace/look around. May use this setup to document future projects while I am working on them.<br></div>');
-var content5short=
-('<br><h3>ROBOTIC ENDOSCOPE CAM</h3><br>')+
-('<div class="contentlandinggriddescription">IT IS WATCHING YOU. :) </div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+$(function () {
+  $('.profile').on('click', function(){
+  $('.description').html('<h3>WELCOME TO MY RESUME!</h3><br> Click on the buttons below to get to know me!');
+});
+});
 
-var content6=
-('<br><h3>CIRCUIT GREETING CARDS</h3><br>')+
-('<div class="contentlandinggriddescription">EXPERIMENTING WITH CONDUCTIVE PAINT</div>')+
-('<div class="contentlandinggriddescription">')+
-('I love the idea of incorperating visible circuits (the "nuts and bolts") into everyday life. Conductive paint was one way that I was experimenting with this.<br></div>');
-var content6short=
-('<br><h3>CIRCUIT GREETING CARDS</h3><br>')+
-('<div class="contentlandinggriddescription">EXPERIMENTING WITH CONDUCTIVE PAINT</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+$(function () {
+  $('#reference').on('click', function(){
+  $('.description').html(reference);
+});
+});
+var reference=('<h3>REFERENCES</h3><br>')+('Lorraine Perry<br><div class="edesc">Library Technician at Kihei Public Library. <br>Phone: (808) 875-6833</div><br> Darius &#39;Bubs&#39; Monsef <div class="edesc">Founder of Nalukai Academy Hacker Camp<br>Email: bubs@monsef.com<br><a id="reflink" href="http://bubs.co/">WEBSITE</a></div><br> Kevin Clarke <div class="edesc">Engineering Services at Goodfellow Bros., Inc.<br>Email: kevinc@goodfellowbros.com</div>');
 
-var content7=
-('<br><h3>ROBOTICS</h3><br>')+
-('<div class="contentlandinggriddescription">HOPEFULLY THEY DO NOT REVOLT</div>')+
-('<div class="contentlandinggriddescription">')+
-('Robotics is a very important part of my life. Not only does it combine my loves of engineering, problem solving, data collection, and everything inbetween, robotics is going to play an important role in the future of humanity. My favorite author just so happens to be Isaac Asimov. So, I take the three laws of robotics very seriously. :)<br></div>');
-var content7short=
-('<br><h3>ROBOTICS</h3><br>')+
-('<div class="contentlandinggriddescription">HOPEFULLY THEY DO NOT REVOLT</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+$(function () {
+  $('#edu').on('click', function(){
+  $('.description').html(edu);
+});
+});
+var edu=('<h3>EDUCATION</h3><br>')+('Kihei Charter High School<br><div class="edesc">2014 - 2018<br>GPA: 4.2 (usually but not currently)</div><br> University of Hawaii Maui College<div class="edesc">2015 - 2018<br>GPA: 3.51</div>');
 
-var content8=
-('<br><h3>LIGHT UP CAT EARS</h3><br>')+
-('<div class="contentlandinggriddescription">CUTE CONVERSATION STARTERS</div>')+
-('<div class="contentlandinggriddescription">')+
-('Easy-to-make cat ears for all occasions. Entirely made from paperclips, wires, LEDs, battery pack/battery/connectors, and lots of tape. I hope to upload a tutorial for them sometime soon, perhaps to Instructables. <br></div>');
-var content8short=
-('<br><h3>LIGHT UP CAT EARS</h3><br>')+
-('<div class="contentlandinggriddescription">CUTE CONVERSATION STARTERS</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content9=
-('<br><h3>BEDROOM GARDEN & GROW LIGHTS</h3><br>')+
-('<div class="contentlandinggriddescription">MY PLANT FRIENDS KEEP ME COMPANY</div>')+
-('<div class="contentlandinggriddescription">')+
-('Started growing some plants in my room during my Junior Year. Currently growing succulents, spinach, cayenne pepper, forget me nots, watermelon, and strawberries. Trying to grow cilantro, kale, spearmint, and catnip. <br></div>');
-var content9short=
-('<br><h3>BEDROOM GARDEN & GROW LIGHTS</h3><br>')+
-('<div class="contentlandinggriddescription">MY PLANT FRIENDS KEEP ME COMPANY</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content10=
-('<br><h3>ROOM AUTOMATION</h3><br>')+
-('<div class="contentlandinggriddescription">INSPIRED BY TONY STARKS HOUSE.</div>')+
-('<div class="contentlandinggriddescription">')+
-('Using an Arudino Mega, bluetooth component, and Android tablet to control various light fixtures and devices that I hoard in my room. Using a Raspberry Pi and a projector to project cheesy Good Morning messages on my wall. Not quite at Iron Man level yet, but I am working on it. :)<br></div>');
-var content10short=
-('<br><h3>ROOM AUTOMATION</h3><br>')+
-('<div class="contentlandinggriddescription">INSPIRED BY TONY STARKS HOUSE.</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content11=
-('<br><h3>REFLECTIONS</h3><br>')+
-('<div class="contentlandinggriddescription">PASSION, AND CHASING DREAMS</div>')+
-('<div class="contentlandinggriddescription">')+
-('I have always known that I wanted to dedicate my life to helping people. I was not always sure how, and at sixteen, I still do not have everything figured out. However, I do know that I want to use my brain, creativity, and passion for learning to contribute something to my fellow inhabitants of this world.<br></div>');
-var content11short=
-('<br><h3>REFLECTIONS</h3><br>')+
-('<div class="contentlandinggriddescription">PASSION, AND CHASING DREAMS</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content12=
-('<br><h3>ART & LITERATURE</h3><br>')+
-('<div class="contentlandinggriddescription">I LOVE THE ARTS, BUT THE ARTS DO NOT ALWAYS LIKE ME</div>')+
-('<div class="contentlandinggriddescription">')+
-('I have enjoyed drawing since a young age, however I am just starting to really get into painting. Painting happens to be a lot harder than drawing, who knew?! I love literature as well. There is nothing I enjoy more than reading a great science fiction book or memoir, however I myself happen to be a terrible writer. Oh well, we all have our strengths. :)<br></div>');
-var content12short=
-('<br><h3>ART & LITERATURE</h3><br>')+
-('<div class="contentlandinggriddescription">I LOVE THE ARTS, BUT THE ARTS DO NOT ALWAYS LIKE ME</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content13=
-('<br><h3>QUIRKY</h3><br>')+
-('<div class="contentlandinggriddescription">FUNNY ENOUGH, IT WORKS.</div>')+
-('<div class="contentlandinggriddescription">')+
-('Little DIY power distribution board that I made from a cork and some nails. I mounted it on a poster for school and received plenty of questions about what it was.<br></div>');
-var content13short=
-('<br><h3>QUIRKY</h3><br>')+
-('<div class="contentlandinggriddescription">FUNNY ENOUGH, IT WORKS.</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content14=
-('<br><h3>SUSTAINABILITY</h3><br>')+
-('<div class="contentlandinggriddescription">I DO NOT WANT TO KILL TURTLES</div>')+
-('<div class="contentlandinggriddescription">')+
-('I brought Starbucks to school and one of my teachers told me that the plastic straws often make it to the ocean and hurt turtles. He proceded to show me a video of a conservationalist pulling a plastic straw out of an injured turtles nose. After learning about that, I decided to be more consious of the plastic waste I produce. As a symbol, I melted down my plastic straw from that day and molded it into the shape of a turtle.<br></div>');
-var content14short=
-('<br><h3>SUSTAINABILITY</h3><br>')+
-('<div class="contentlandinggriddescription">I DO NOT WANT TO KILL TURTLES</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content15=
-('<br><h3>3D PRINTING</h3><br>')+
-('<div class="contentlandinggriddescription">DONT FORGET TO BE AWESOME</div>')+
-('<div class="contentlandinggriddescription">')+
-('Saved up and bought a 3d printer during my Junior Year. One of the very first designs that I made/printed was a necklace with the acronym DFTBA (Dont Forget To Be Awesome).<br></div>');
-var content15short=
-('<br><h3>3D PRINTING</h3><br>')+
-('<div class="contentlandinggriddescription">DONT FORGET TO BE AWESOME</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
-
-var content16=
-('<br><h3>PROGRAMMING</h3><br>')+
-('<div class="contentlandinggriddescription">CREATIVE CODING</div>')+
-('<div class="contentlandinggriddescription">')+
-('Like many people, I enjoy programming as a hobby. One of the main reasons why I love the world of programming is that it is inherently creative and collaborative. With the tools and constraints you are allowed, you can solve all sorts of problems and create beautiful results. I consider it to be an art, but the type of art where you are encouraged to build upon the creations of other artists. That is why it is so powerful. <br></div>');
-var content16short=
-('<br><h3>PROGRAMMING</h3><br>')+
-('<div class="contentlandinggriddescription">CREATIVE CODING</div>')+
-('<div class="contentlandinggriddescription">')+
-('<i>Click For More Info</i><br></div>');
+var skill1=('Proficient in HTML, CSS, Javascript, and C.<br>Fairly experienced in Java, C++, and Python.<br>Good at using the command line in UNIX based operating systems<br>Can pick up new languages quickly.');
+var skill2=('Proficient at wiring, soldering, and building + designing circuits. Very experienced with Arduino and Raspberry Pi.<br>Able to use oscilloscopes, multimeters, power supplies, and other electronics equipment.');
+var skill3=('Proficient at 3D modeling using Autodesk Inventor, Fusion 360, Sketchup. Experienced with  Civil 3d and Blender. Some graphic design experience.');
+var skill4=('Friendly, likes to smile at people.');
+$(function () {
+  $('#skills').on('click', function(){
+  $('.description').html(skills);
+  $('#s1').hide();
+  $('#s2').hide();
+  $('#s3').hide();
+  $('#s4').hide();
+  $(function () {
+    $('#skill1').on('click', function(){
+$('#s1').html(skill1).show();
+$('#s2').hide();
+$('#s3').hide();
+$('#s4').hide();
+  });
+  $('#skill2').on('click', function(){
+$('#s2').html(skill2).show();
+$('#s1').hide();
+$('#s3').hide();
+$('#s4').hide();
+});
+$('#skill3').on('click', function(){
+$('#s3').html(skill3).show();
+$('#s1').hide();
+$('#s2').hide();
+$('#s4').hide();
+});
+$('#skill4').on('click', function(){
+$('#s4').html(skill4).show();
+$('#s1').hide();
+$('#s2').hide();
+$('#s3').hide();
+});
+  });
+});
+});
+var skills=('<h3>SKILLS</h3><br>')+('<ul class="skills "><li><button class="moreinfo" id="skill1"><span>Programming</span></button></li>')+('<div class="wdesc" id="s1"></div>')+
+('<li><button class="moreinfo" id="skill2"><span>Engineering</span></button></li>')+('<div class="wdesc" id="s2"></div>')+
+('<li><button class="moreinfo" id="skill3"><span>General STEM</span></button></li>')+('<div class="wdesc" id="s3"></div>')+
+('<li><button class="moreinfo" id="skill4"><span>Other</span></button></li>')+('<div class="wdesc" id="s4"></div>')+('</ul>');
